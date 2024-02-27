99c9ea610f3c0e2d7c6315df3755709f03b7f717
dqmdz <daniel.quinterospinto@gmail.com>
2024-02-27 16:21:40 -0300

    First commit

---
+++ src/test/java/ar/edu/um/tesoreria/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.um.tesoreria.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
